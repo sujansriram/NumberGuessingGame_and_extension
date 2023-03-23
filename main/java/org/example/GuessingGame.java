d9b4f37e7f7d0805312ffabf3adbb25a840a16a5
Sujan Sriram <sujansriram9@gmail.com>
2023-03-23 23:44:42 +0000

    updated GuessingGame with while loop and random number generator

--- main/java/org/example/GuessingGame.java
+++ main/java/org/example/GuessingGame.java
@@ -6,18 +6,36 @@ import java.util.Random;
 public class GuessingGame {
     public static void main(String[] args) {
 
-//        int randomNumber = Math.random();
-        int secretNumber = 69;
+        System.out.println("Guess the number from 1 to 1000");
+//        random number generator
+        Random randomNumber = new Random();
+        int secretNumber = randomNumber.nextInt(1000);
 
-        Scanner reader = new Scanner(System.in);
-        int guessedNumber = reader.nextInt();
+//        assign the guessed number a value outside the generator bound so the while loop starts off true
+        int guessedNumber = 1001;
 
-        if (guessedNumber == secretNumber){
-            System.out.println("correct!");
-        } else if (guessedNumber < secretNumber) {
-            System.out.println("Wrong! Too Small");
-        } else {
-            System.out.println("Wrong! Too Big");
+//        NEW GUESSING GAME (ALLOWS UNLIMITED GUESSES)
+        while (guessedNumber != secretNumber){
+            Scanner reader = new Scanner(System.in);
+            guessedNumber = reader.nextInt();
+            if (guessedNumber < secretNumber) {
+                System.out.println("Wrong! Too Small");
+            } else if (guessedNumber > secretNumber) {
+                System.out.println("Wrong! Too Big");
+            } else {
+                System.out.println("correct!");
+            }
         }
     }
 }
+//        OLD GUESSING GAME (RUBBISH! ONLY ONE GUESS)
+//        Scanner reader = new Scanner(System.in);
+//        int guessedNumber = reader.nextInt();
+
+//        if (guessedNumber == secretNumber){
+//            System.out.println("correct!");
+//        } else if (guessedNumber < secretNumber) {
+//            System.out.println("Wrong! Too Small");
+//        } else {
+//            System.out.println("Wrong! Too Big");
+//        }
